aea49bb1f271aa7c06b94dfb56b4599e78e9e7ea
Alexey Kopievskiy <alexeykopievskiy@gmail.com>
2020-04-14 13:26:14 +0300

    done contacts page

--- pages/pages/_name.vue
+++ pages/pages/_name.vue
@@ -2,20 +2,20 @@
   <main class="v-fall-main">
     <div class="v-fall__container grid grid-column v-fall__container--contacts">
       <section class="v-fall__container--white v-fall-contacts__block">
-        <h2 class="v-fall-contacts__header">Контакты</h2>
-        <h4
-          class="v-fall-contacts__subheader"
-        >Если у вас есть вопросы, предложения или комментарии о сайте, напишите нам по адресу wearevideout@gmail.com</h4>
-        <p
-          class="v-fall-contacts__text"
-        >Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est eopksio laborum. Sed ut perspiciatis unde omnis istpoe natus error sit voluptatem accusantium doloremque eopsloi laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunot explicabo. Nemo ernim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sedopk quia consequuntur magni dolores eos qui rationesopl voluptatem sequi nesciunt. Neque porro quisquameo est, qui dolorem ipsum quia dolor sit amet, eopsmiep consectetur, adipisci velit, seisud quia non numquam eius modi tempora incidunt ut labore et dolore wopeir magnam aliquam quaerat voluptatem eoplmuriquisqu</p>
-      </section>
-      <section class="v-fall-contacts__block">
-        <h3 class="v-fall-contacts__subheader">Редакция</h3>
-        <p
-          class="v-fall-contacts__subheader"
-        >Пишите сюда team@videout.ru, чтобы связаться с редакцией сайта. Не забудьте указать свои координаты — ваше имя, компанию и номер телефона.</p>
+        <h2 class="v-fall-contacts__header">{{name}}</h2>
+        <div class="v-fall-contacts__content" v-html="content"></div>
       </section>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const { name, content } = await $axios.$get(
+      "https://api.videout.ru/pages/" + params.name
+    );
+    return { name, content };
+  }
+};
+</script>
